e7aba50347d2641996147791656f39343439fcf0
Victor Rocha <victor.dearruda.rocha@gmail.com>
2025-03-27 08:27:22 -0300

    Try e catches

--- ProjetoBanco/Program.cs
+++ ProjetoBanco/Program.cs
@@ -7,6 +7,8 @@ namespace ProjetoBanco
     {
         static void Main(string[] args)
         {
+            try 
+            {
             List<ContaBancaria> contaBancarias = new List<ContaBancaria>();
 
             ContaCorrente cC = new ContaCorrente("Victor de Arruda", 242, 1000);
@@ -34,50 +36,59 @@ namespace ProjetoBanco
 
 
             int continua = 0;
-            while (continua == 0) 
-            {
-                Console.Clear();
-                Console.WriteLine();
-                Console.WriteLine("__________________________________");
-                Console.WriteLine("        Sistema bancário          ");
-                Console.WriteLine("----------------------------------");
-                Console.WriteLine();
-                Console.WriteLine("Digite: ");
-                Console.WriteLine("1 - Criar nova conta");
-                Console.WriteLine("2 - Sacar");
-                Console.WriteLine("3 - Depositar ");
-                Console.WriteLine("4 - Exibir saldo ");
-                Console.WriteLine("Qualquer outra coisa para sair");
-                opc = Console.ReadLine();
-
-                switch (opc)
+                while (continua == 0)
                 {
-                    case "1":
-                        CriarConta(contaBancarias);
-                        break;
+                    Console.Clear();
+                    Console.WriteLine();
+                    Console.WriteLine("__________________________________");
+                    Console.WriteLine("        Sistema bancário          ");
+                    Console.WriteLine("----------------------------------");
+                    Console.WriteLine();
+                    Console.WriteLine("Digite: ");
+                    Console.WriteLine("1 - Criar nova conta");
+                    Console.WriteLine("2 - Sacar");
+                    Console.WriteLine("3 - Depositar ");
+                    Console.WriteLine("4 - Exibir saldo ");
+                    Console.WriteLine("Qualquer outra coisa para sair");
+                    opc = Console.ReadLine();
 
-                    case "2":
-                        Sacar(contaBancarias);
-                        break;
+                    switch (opc)
+                    {
+                        case "1":
+                            CriarConta(contaBancarias);
+                            break;
 
-                    case "3":
-                        Depositar(contaBancarias);
-                        break;
+                        case "2":
+                            Sacar(contaBancarias);
+                            break;
 
-                    case "4":
-                        MostrarSaldo(contaBancarias);
-                        break;
+                        case "3":
+                            Depositar(contaBancarias);
+                            break;
 
-                    default:
-                        Console.WriteLine("Sistema encerrado.");
-                        break;
+                        case "4":
+                            MostrarSaldo(contaBancarias);
+                            break;
+
+                        default:
+                            Console.WriteLine("Sistema encerrado.");
+                            break;
+                    }
+
+                    Console.WriteLine("Você deseja continuar?");
+                    Console.WriteLine("0 - Sim | 1 - Não");
+                    continua = int.Parse(Console.ReadLine());
                 }
-
-                Console.WriteLine("Você deseja continuar?");
-                Console.WriteLine("0 - Sim | 1 - Não");
-                continua = int.Parse(Console.ReadLine());
-
             }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Insira o formato de dado correto.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+
 
         }
 
@@ -140,83 +151,107 @@ namespace ProjetoBanco
             }
             catch (Exception ex)
             {
-
+                Console.WriteLine(ex.Message);
             }
         }
 
         public static bool ProcurarContas(int num, List<ContaBancaria> contaBancarias)
         {
-            foreach (var i in contaBancarias)
-            {
-                if(num == i.NumeroConta)
+
+                foreach (var i in contaBancarias)
                 {
-                    return true;
-                } 
-            }
-            return false;
+                    if (num == i.NumeroConta)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+            
         }
 
         public static void Sacar(List<ContaBancaria> contaBancarias)
         {
-            Console.WriteLine("Escolha a conta em que você deseja sacar: ");
-            MostrarContas(contaBancarias);
-            Console.WriteLine("Numero da conta: ");
-            int opc = int.Parse((Console.ReadLine()));
-            ProcurarContas(opc, contaBancarias);
-            if (ProcurarContas(opc, contaBancarias))
+            try
             {
-                ContaBancaria conta = contaBancarias.FirstOrDefault(c => c.NumeroConta == opc);
-                Console.WriteLine("Quanto você deseja sacar? ");
-                double sacar = double.Parse(Console.ReadLine());
-                conta.Sacar(sacar);
+                Console.WriteLine("Escolha a conta em que você deseja sacar: ");
+                MostrarContas(contaBancarias);
+                Console.WriteLine("Numero da conta: ");
+                int opc = int.Parse((Console.ReadLine()));
+                ProcurarContas(opc, contaBancarias);
+                if (ProcurarContas(opc, contaBancarias))
+                {
+                    ContaBancaria conta = contaBancarias.FirstOrDefault(c => c.NumeroConta == opc);
+                    Console.WriteLine("Quanto você deseja sacar? ");
+                    double sacar = double.Parse(Console.ReadLine());
+                    conta.Sacar(sacar);
+                }
+                else
+                {
+                    Console.WriteLine("Conta inexistente");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("Conta inexistente");
+                Console.WriteLine(ex.ToString());
             }
-
 
 
         }
 
         public static void MostrarSaldo(List<ContaBancaria> contaBancarias)
         {
-            Console.WriteLine("Escolha a conta em que você deseja sacar: ");
-            MostrarContas(contaBancarias);
-            Console.WriteLine("Numero da conta: ");
-            int opc = int.Parse((Console.ReadLine()));
-            ProcurarContas(opc, contaBancarias);
-            if (ProcurarContas(opc, contaBancarias))
+            try
             {
-                Console.WriteLine();
-                ContaBancaria conta = contaBancarias.FirstOrDefault(c => c.NumeroConta == opc);
-                conta.ExibirSaldo();
+                Console.WriteLine("Escolha a conta em que você deseja sacar: ");
+                MostrarContas(contaBancarias);
+                Console.WriteLine("Numero da conta: ");
+                int opc = int.Parse((Console.ReadLine()));
+                ProcurarContas(opc, contaBancarias);
+                if (ProcurarContas(opc, contaBancarias))
+                {
+                    Console.WriteLine();
+                    ContaBancaria conta = contaBancarias.FirstOrDefault(c => c.NumeroConta == opc);
+                    conta.ExibirSaldo();
+                }
+                else
+                {
+                    Console.WriteLine("Conta inexistente");
+                }
             }
-            else
+            catch(Exception ex)
             {
-                Console.WriteLine("Conta inexistente");
+                Console.WriteLine(ex.ToString());
             }
-        }
+            
+            }   
 
         public static void Depositar(List<ContaBancaria> contaBancarias)
         {
-            Console.WriteLine("Escolha a conta que deseja realizar o deposito");
-            MostrarContas(contaBancarias);
+            try
+            {
+                Console.WriteLine("Escolha a conta que deseja realizar o deposito");
+                MostrarContas(contaBancarias);
 
-            Console.WriteLine("Numero da conta: ");
-            int opc = int.Parse((Console.ReadLine()));
-            ProcurarContas(opc, contaBancarias);
-            if (ProcurarContas(opc, contaBancarias))
-            {
-                ContaBancaria conta = contaBancarias.FirstOrDefault(c => c.NumeroConta == opc);
-                Console.WriteLine("Quanto você deseja sacar? ");
-                double sacar = double.Parse(Console.ReadLine());
-                conta.Depositar(sacar);
+                Console.WriteLine("Numero da conta: ");
+                int opc = int.Parse((Console.ReadLine()));
+                ProcurarContas(opc, contaBancarias);
+                if (ProcurarContas(opc, contaBancarias))
+                {
+                    ContaBancaria conta = contaBancarias.FirstOrDefault(c => c.NumeroConta == opc);
+                    Console.WriteLine("Quanto você deseja depositar? ");
+                    double deposito = double.Parse(Console.ReadLine());
+                    conta.Depositar(deposito);
+                }
+                else
+                {
+                    Console.WriteLine("Conta inexistente");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("Conta inexistente");
+                Console.WriteLine(ex.ToString());
             }
-        }
+            }
     }   
 }
